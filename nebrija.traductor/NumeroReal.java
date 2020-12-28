3665fc1b8d5e5361a1e4f9f96c998c5de62b09a7
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-28 21:01:56 +0100

    Create NumeroReal.java

---
+++ nebrija.traductor/NumeroReal.java
@@ -0,0 +1,23 @@
+package nebrija.traductor;
+
+public class NumeroReal extends ComponenteLexico {
+
+	private float valor;
+
+	public NumeroReal(float valor) {
+		
+		super("float");
+		this.valor = valor;
+	}
+
+	public float getValor() {
+		
+		return this.valor;
+
+	}
+
+	public String toString() {
+
+		return super.toString() + ", " + this.valor;
+	}
+}
