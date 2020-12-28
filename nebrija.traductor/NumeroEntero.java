1e4931764eb171a8ccca9cbb2fe0d402ddf96522
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-28 21:01:25 +0100

    Create NumeroEntero.java

---
+++ nebrija.traductor/NumeroEntero.java
@@ -0,0 +1,22 @@
+package nebrija.traductor;
+
+public class NumeroEntero extends ComponenteLexico{
+	
+	private int valor;
+
+	public NumeroEntero(int valor) {
+
+		super("int");
+		this.valor = valor;
+	}
+
+	public int getValor() {
+
+		return this.valor;
+	}
+
+	public String toString() {
+
+		return super.toString() + ", " + this.valor;
+	}
+}
