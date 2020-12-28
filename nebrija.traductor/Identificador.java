09947c407d179a68f951a909ac290ae0e062c12f
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-28 21:00:35 +0100

    Create Identificador.java

---
+++ nebrija.traductor/Identificador.java
@@ -0,0 +1,25 @@
+package nebrija.traductor;
+
+public class Identificador extends ComponenteLexico{
+
+	private String lexema;
+
+	public Identificador(String lexema) {
+		
+		super("id");
+		this.lexema = lexema;
+
+	}
+
+	public String getLexema() {
+		
+		return this.lexema;
+
+	}
+
+	public String toString() {
+
+		
+		return super.toString() + ", " + this.lexema;
+	}
+}
