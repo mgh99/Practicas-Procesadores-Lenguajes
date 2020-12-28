2cb3ec98d55c8310903734c0d5724d21e80c8958
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-28 20:58:55 +0100

    Create TipoPrimitivo.java

---
+++ practica_4/TipoPrimitivo.java
@@ -0,0 +1,12 @@
+package practica_4;
+
+public class TipoPrimitivo extends TipoDato {
+	
+	public TipoPrimitivo(String tipo) {
+		super(tipo);
+	}
+
+	public String toString() {
+		return super.getTipo();
+	}
+}
