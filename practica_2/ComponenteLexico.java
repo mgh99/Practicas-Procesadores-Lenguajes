4edad6b2b7fb3d8acfa39009d3f868df2e425095
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-28 20:47:27 +0100

    Create ComponenteLexico.java

---
+++ practica_2/ComponenteLexico.java
@@ -0,0 +1,44 @@
+package practica_2;
+
+public class ComponenteLexico {
+
+	private String etiqueta;
+	private String valor;
+
+	public ComponenteLexico(String etiqueta) {
+		
+		this.etiqueta = etiqueta;
+		this.valor = "";
+	}
+	
+	public ComponenteLexico(float f) {
+		
+	}
+	
+	public ComponenteLexico(String etiqueta, String valor) {
+		
+		this.etiqueta = etiqueta;
+		this.valor = valor;
+	}
+
+	public String getEtiqueta() {
+
+		return this.etiqueta;
+	}
+
+	public String getValor() {
+		return this.valor;
+	}
+
+	public String toString() {
+
+		if (this.valor.length() == 0) {
+			
+			return "<" + this.etiqueta + ">";
+			
+		}else {
+			
+			return "<" + this.etiqueta + ">";
+		}
+	}
+}
