590ec93f2cbfad1b6e8810fe3c4ba82b98fe359b
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-28 20:47:54 +0100

    Create Identificador.java

---
+++ practica_2/Identificador.java
@@ -0,0 +1,25 @@
+package practica_2;
+
+public class Identificador extends ComponenteLexico{
+
+	private String lexema;
+
+	public Identificador(String lexema) {
+		
+		super("id");
+		this.lexema = lexema;
+
+	}
+
+	public String getLexema() {
+		
+		return this.lexema;
+
+	}
+
+	public String toString() {
+
+		
+		return super.toString() + ", " + this.lexema;
+	}
+}
