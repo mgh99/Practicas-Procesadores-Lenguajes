5d6efdf027d825e21621d6483f952c6814eaf29f
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-14 13:13:08 +0100

    Create TipoArray.java

---
+++ Caso_practico/TipoArray.java
@@ -0,0 +1,17 @@
+public class TipoArray extends TipoDato {
+
+	private int tamaño;
+	
+	public TipoArray(String tipo, int tamaño) {
+		super(tipo);
+		this.tamaño = tamaño;
+	}
+	
+	public int getTamaño() {
+		return this.tamaño;
+	}
+	
+	public String toString() {
+		return "array (" + super.toString() + ", " + this.tamaño + ")";
+	}
+}
