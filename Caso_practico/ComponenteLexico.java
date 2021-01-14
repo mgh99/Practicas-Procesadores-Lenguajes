7ccf66fd29dd7fd9b55fbf2fd15b08b32181d353
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-14 13:05:27 +0100

    Create ComponenteLexico.java

---
+++ Caso_practico/ComponenteLexico.java
@@ -0,0 +1,16 @@
+public class ComponenteLexico {
+		
+		private String etiqueta;
+		
+		public ComponenteLexico(String etiqueta) {
+			this.etiqueta = etiqueta;
+		}
+		
+		public String getEtiqueta() {
+			return this.etiqueta;
+		}
+
+		public String toString() {
+			return this.etiqueta;
+		}
+}
