12b6238ab301464e2f39a26741734fbd4837c494
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-14 13:06:59 +0100

    Create Identificador.java

---
+++ Caso_practico/Identificador.java
@@ -0,0 +1,18 @@
+public class Identificador  extends ComponenteLexico{
+
+	private String lexema;
+
+	public Identificador(String lexema) {
+		super("id");
+		this.lexema = lexema;
+	}
+
+	public String getLexema() {
+		return this.lexema;
+	}
+	
+	public String toString() {
+		return super.toString()+","+this.getLexema();
+	} 
+
+}
