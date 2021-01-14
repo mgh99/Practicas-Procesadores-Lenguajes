41db4f48508a7c4ffa62a7071eb5c25b2fb55a84
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-14 13:12:12 +0100

    Create NumeroReal.java

---
+++ Caso_practico/NumeroReal.java
@@ -0,0 +1,18 @@
+public class NumeroReal extends ComponenteLexico {
+
+	private float valor;
+
+	public NumeroReal(float valor) {
+		super("float");
+		this.valor = valor;
+	}
+
+	/*public float getValor() {
+		return valor;
+	}*/
+
+	public String toString() {
+		return super.toString() + ", " + this.valor;
+	}
+
+}
